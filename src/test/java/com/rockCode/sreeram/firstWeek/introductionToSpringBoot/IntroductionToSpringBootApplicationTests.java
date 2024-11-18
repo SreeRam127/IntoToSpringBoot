d9248bf1cd3b1cec880fbca276a9b97697de5908
SreeRam127 <sreeramchray@gmail.com>
2024-11-18 19:50:26 +0530

    code initializer

---
+++ src/test/java/com/rockCode/sreeram/firstWeek/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockCode.sreeram.firstWeek.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
